a9d400f5d4089c43ba319c66eed69c701f8a91e2
gabilbck <gabrielieduardalembeck@gmail.com>
2024-06-21 16:00:12 -0300

    add

---
+++ Web/src/js/scriptCreate.js
@@ -0,0 +1,14 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../src/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "../src/img/close_white_36dp.svg";
+    }
+}
+
+function search(){
+    alert('Estamos trablhando para disponibilizar a função assim que possível!');
+}
--- Web/src/js/scriptEvents.js
+++ Web/src/js/scriptEvents.js
@@ -1,4 +1,4 @@
-function menuShows() {
+function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
@@ -12,45 +12,3 @@ function menuShows() {
 function search(){
     alert('Estamos trablhando para disponibilizar a função assim que possível!');
 }
-
-let count = 1;
-document.getElementById("radio1").checked = true;
-setInterval( function() {
-    nextImage();
-}, 5000);
-
-function nextImage(){
-    count++;
-    if(count > 4){
-        count = 1;
-    }    
-    document.getElementById("radio"+count).checked = true;
-}
-
-
-// function pullHeader(){
-//     fetch('../view/template/header.html')
-//     .then(response => response.text())
-//     .then(html => {
-//         document.getElementById('header').innerHTML = html;
-//     })
-//     .catch(error => console.error('Erro ao carregar o cabeçalho:', error));
-// }
-
-// window.addEventListener('load', pullHeader);
-
-// function includeHeader() {
-//     const xhr = new XMLHttpRequest();
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState === XMLHttpRequest.DONE) {
-//             if (xhr.status === 200) {
-//                 document.getElementById('header').innerHTML = xhr.responseText;
-//             } else {
-//                 console.error('Erro ao carregar o cabeçalho:', xhr.status);
-//             }
-//         }
-//     };
-//     xhr.open('GET', '../view/template/header.html', true);
-//     xhr.send();
-// }
-// window.addEventListener('load', includeHeader);
