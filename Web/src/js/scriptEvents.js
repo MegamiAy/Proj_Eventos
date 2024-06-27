bcab2314e079577abc62d0d3ca44659455ce2163
gabilbck <gabrielieduardalembeck@gmail.com>
2024-06-27 14:50:10 -0300

    arrumei a movimentação do events

--- Web/src/js/scriptEvents.js
+++ Web/src/js/scriptEvents.js
@@ -1,4 +1,3 @@
-// Função para rolar os cards
 document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('.category-section');
     sections.forEach(section => {
@@ -32,5 +31,42 @@ document.addEventListener('DOMContentLoaded', function() {
                 behavior: 'smooth'
             });
         });
+
+        // Função para dispositivos móveis
+        if (window.innerWidth <= 767) {
+            let touchStartX = 0;
+            let touchEndX = 0;
+
+            cardsContainer.addEventListener('touchstart', function(event) {
+                touchStartX = event.changedTouches[0].screenX;
+            });
+
+            cardsContainer.addEventListener('touchmove', function(event) {
+                touchEndX = event.changedTouches[0].screenX;
+            });
+
+            cardsContainer.addEventListener('touchend', function() {
+                const maxScrollLeft = cardsContainer.scrollWidth - cardsContainer.clientWidth;
+                const cardScrollStep = cardWidth; // Rola um card por vez
+
+                if (touchEndX < touchStartX) { // Rolar para a esquerda
+                    scrollAmount += cardScrollStep;
+                    if (scrollAmount > maxScrollLeft) {
+                        scrollAmount = maxScrollLeft;
+                    }
+                } else if (touchEndX > touchStartX) { // Rolar para a direita
+                    scrollAmount -= cardScrollStep;
+                    if (scrollAmount < 0) {
+                        scrollAmount = 0;
+                    }
+                }
+
+                cardsContainer.scrollTo({
+                    top: 0,
+                    left: scrollAmount,
+                    behavior: 'smooth'
+                });
+            });
+        }
     });
 });
